82a9a67e907267ba3768814a6008e9c02cd00da8
Lynettew <lwobig@bigboxevents.com>
2026-05-19 00:45:08 -0500

    add supabase client

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(https://zanfhsqyzefmgzvdhpbb.supabase.co, eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphbmZoc3F5emVmbWd6dmRocGJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNDQxMDgsImV4cCI6MjA5NDcyMDEwOH0.SXBp9id0vpzfjVJt6v7j9pougBrhagaJGHBsoTGRkds);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
